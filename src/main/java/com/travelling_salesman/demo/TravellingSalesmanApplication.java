ed874336ef7d638cb4bc794650e40820b433fafe
Meelis <meelis.tapo@gmail.com>
2018-03-14 17:21:37 +0200

    Autowired initial entries

--- src/main/java/com/travelling_salesman/demo/TravellingSalesmanApplication.java
+++ src/main/java/com/travelling_salesman/demo/TravellingSalesmanApplication.java
@@ -1,8 +1,8 @@
 package com.travelling_salesman.demo;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,13 +13,16 @@ public class TravellingSalesmanApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(TravellingSalesmanApplication.class);
 
+	@Autowired
+	private LocationRepository locationRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TravellingSalesmanApplication.class, args);
 	}
 
 
 	@Bean
-	CommandLineRunner init(LocationRepository locationRepository) {
+	CommandLineRunner init() {
 		return (args) ->{
 			locationRepository.save(new Location("Tallinn",59.26,24.45));
 			locationRepository.save(new Location("Tartu",58.23,26.43));
